61f2047fcbdde7f41966ea21aa4166929427e7df
Alex Sircu <alex.sricu@gmail.com>
2020-11-11 16:31:23 +0100

    Alcune correzioni

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ console.log(numberKm)
 
 var age = parseInt(prompt("Qual è la sua età?"))
 console.log(age)
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km (0.21 € al km);
 var ticketPricePerKm = 0.21;
 
@@ -12,15 +11,15 @@ var ticketPrice = parseInt(numberKm * ticketPricePerKm);
 console.log(ticketPrice);
 
 var tcketPriceDiscount;
-document.getElementById("price").innerHTML = ticketPrice;
-// va applicato uno sconto del 20% per i minorenni;
+
 if (age < 18) {
+  // va applicato uno sconto del 20% per i minorenni;
   ticketPriceDiscount = ticketPrice - ((ticketPrice * 20) / 100);
 } else if (age > 65) {
+  // va applicato uno sconto del 40% per gli over 65.
   ticketPriceDiscount = ticketPrice - ((ticketPrice * 40) / 100);
 } else {
   ticketPriceDiscount = ticketPrice;
 }
 console.log(ticketPriceDiscount);
 document.getElementById("price").innerHTML = ticketPriceDiscount;
-// va applicato uno sconto del 40% per gli over 65.
